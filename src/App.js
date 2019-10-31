bd0d744dba7e426fdea18faeb0be4aeebfb2aa71
DOMINIC BRICE <dominicbrice@MBP-de-DOMINIC.lan>
2019-10-31 10:22:06 +0100

    footer

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import WithCard from './Components/WithCard'
 
 import './CSS/App.css'
+import Footer from './Components/footer';
 
 function App() {
   return (
     <>
       <WithCard />
+      <Footer />
     </>
   );
 }
