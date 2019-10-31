e151da75e5bdebd1bd8fbb6311733ad733b9da26
Superdref <frederiquemendy2205@gmail.com>
2019-10-31 10:18:43 +0100

    changed img overlay div size, centered title movie or trick

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import WithCard from './Components/WithCard'
+import WithCard from './Components/WithCard';
+import Header from './Components/Header.jsx';
 
 import './CSS/App.css'
 
 function App() {
   return (
     <>
+      <Header />
       <WithCard />
     </>
   );
